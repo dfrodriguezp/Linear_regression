4c97861daaa53ff410563fe7a2a98fc1623221d0
Daniel Felipe Rodríguez Patiño <dfrodriguezp@unal.edu.co>
2019-02-03 14:37:50 -0500

    Plot results

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy
 from scipy import stats
+from matplotlib import pyplot
 import os
 import sys
 
@@ -8,8 +9,11 @@ def dataIn(var):
     return numpy.array(data.split(", "), dtype=float)
 
 def main():
-    y = dataIn("y")
-    x = dataIn("x")
+    # y = dataIn("y")
+    # x = dataIn("x")
+
+    y = numpy.arange(200, 700, 100)
+    x = numpy.array([0.1556, 0.2181, 0.2767, 0.4058, 0.4576])
 
     if len(y) != len(x):
         os.system("clear")
@@ -26,6 +30,20 @@ def main():
     print("\nm = %f" %(m))
     print("b = %f" %(b))
     print("r = %f\n" %(r))
+
+    newX = numpy.linspace(min(x), max(x), 100)
+    newY = m * newX + b
+
+    pyplot.figure()
+    pyplot.scatter(x, y, label="Raw data")
+    pyplot.plot(newX, newY, label="Linear regression")
+    pyplot.xlabel("$x$", fontsize=20)
+    pyplot.ylabel("$y$", fontsize=20)
+    pyplot.legend(loc="best")
+    pyplot.grid()
+    pyplot.savefig("graph.pdf")
+    pyplot.show()
+    pyplot.close()
 
 if __name__ == '__main__':
     main()
